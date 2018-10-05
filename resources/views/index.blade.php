0fd11b0454f43f57e05059e12b09d2dce68d11db
SJW <sverhoeve@davinci.nl>
2018-10-05 14:02:54 +0200

    readded index.blade.php

---
+++ resources/views/index.blade.php
@@ -0,0 +1,13 @@
+@extends ('layouts.landing.master')
+
+@section ('content')
+    <!-- Content -->
+    <div class="bg-white">
+        <section class="content content-boxed">
+            <div class="card">
+
+            </div>
+        </section>
+    </div>
+    <!-- END Content 4 -->
+@endsection
