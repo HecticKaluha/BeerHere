593e836b71396ae3608fe9125ad72d28e81dab2f
SJW <sverhoeve@davinci.nl>
2018-10-12 09:54:24 +0200

    Fixed login button bug

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -113,7 +113,7 @@
 <!-- OneUI Core JS: jQuery, Bootstrap, slimScroll, scrollLock, Appear, CountTo, Placeholder, Cookie and App.js -->
 <script src="{{asset('assets/js/core/jquery.min.js')}}"></script>
 <script src="{{asset('assets/js/oneui.min.js')}}"></script>
-<script src="{{asset('assets/js/core/bootstrap.min.js')}}"></script>
+{{--<script src="{{asset('assets/js/core/bootstrap.min.js')}}"></script>--}}
 <script src="{{asset('assets/js/core/jquery.slimscroll.min.js')}}"></script>
 <script src="{{asset('assets/js/core/jquery.scrollLock.min.js')}}"></script>
 <script src="{{asset('assets/js/core/jquery.appear.min.js')}}"></script>
