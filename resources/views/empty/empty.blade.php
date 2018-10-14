2eb0342b7c04d1b510e312931138fcf2ad0a9f6b
SJW <sverhoeve@davinci.nl>
2018-10-14 16:46:59 +0200

    added link to suggestions page when no matches or likes

--- resources/views/empty/empty.blade.php
+++ resources/views/empty/empty.blade.php
@@ -2,3 +2,5 @@
     <p>Don't be sad because it didn't happen, smile because it can still happen.</p>
     <footer>Stefano Verhoeve</footer>
 </blockquote>
+
+<a href="/suggestions">Click here to meet new people</a>
